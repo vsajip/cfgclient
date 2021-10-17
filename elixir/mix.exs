f91b69dc5f88a5a17206ff3d9bdb2ef20b92ea93
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2021-10-17 16:31:20 +0100

    Correct errors in workflow and Elixir app settings.

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule CFGClient.MixProject do
     [
       app: :cfg_client,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       escript: [main_module: CFGClient],
       deps: deps()
